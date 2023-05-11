34e329311d25b5156c94097efa3a2d5815bc7a8d
arihanv <arihanvaranasi@gmail.com>
2023-05-11 02:19:32 -0500

    Improved Responsiveness and Added Skills

--- src/Special.js
+++ src/Special.js
@@ -5,14 +5,25 @@ import TopBar from "./TopBar";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import { FormControl } from "react-bootstrap";
-import { ClockFill, Search, ArrowRightCircleFill, Reception4, Reception3, Reception2, Person, PersonFill, PlusCircleFill } from "react-bootstrap-icons";
+import {
+  ClockFill,
+  Search,
+  ArrowRightCircleFill,
+  Reception4,
+  Reception3,
+  Reception2,
+  Person,
+  PersonFill,
+  PlusCircleFill,
+} from "react-bootstrap-icons";
 import gradient from "random-gradient";
 
 function Special() {
   const courses = [
     {
       name: "Machine Learning",
-      description: "Learn Machine Learning from the best in the industry through our advanced course",
+      description:
+        "Learn Machine Learning from the best in the industry through our advanced course",
     },
     {
       name: "Web Development",
@@ -33,7 +44,8 @@ function Special() {
     },
     {
       name: "Cyber Security",
-      description: "Learn Cyber Security through various lessons from licensed instructors",
+      description:
+        "Learn Cyber Security through various lessons from licensed instructors",
     },
     {
       name: "Ethical Hacking",
@@ -68,46 +80,68 @@ function Special() {
             </div>
             <div className="grid justify-center gap-3 lg:grid-cols-2">
               {courses.map((course) => (
-                <div className="bg-gray-900 grid grid-child min-h-[150px] 2xl:h-[300px] rounded-xl gap-4 p-3">
+                <div className="bg-gray-900 grid grid-child min-h-fit 2xl:h-[300px] lg:grid-cols-1 grid-cols-[1fr_2fr] xl:grid-cols-[1fr_2fr] rounded-xl gap-4 p-3">
                   <div
                     className="flex block rounded-tr-[5rem] lg:hidden xl:block h-full min-w-[5rem] gradSquare max-h-[100%]"
                     style={{ background: gradient(course.name) }}
                   />
-                  <div className="flex flex-col gap-2">
-                    <div className="mb-1 text-lg font-bold text-left 2xl:text-2xl flex items-center justify-between">
+                  <div className="flex flex-col gap-2.5">
+                    <div className="flex items-center justify-between mb-1 text-lg font-bold text-left 2xl:text-2xl">
                       {course.name}
                       <button>
-                      <PlusCircleFill/>
+                        <PlusCircleFill />
                       </button>
                     </div>
                     <div className="text-sm text-left 2xl:text-lg">
-                        {course.description}
+                      {course.description}
                     </div>
-                    <div className="bg-gray-800 whitespace-pre rounded-xl flex flex-row p-2 font-semibold gap-4">
+                    <div className="flex flex-row gap-4 p-2 font-semibold whitespace-pre bg-gray-800 rounded-xl">
                       <div className="text-[1rem]  2xl:text-[1.2rem] flex flex-row gap-2 items-center">
-                        <ClockFill />
-                        3 Months
+                        <ClockFill />3 Months
                       </div>
                       <div className="text-[1rem] 2xl:text-[1.2rem] flex flex-row gap-2 items-center">
-                        <Reception4/>
+                        <Reception4 />
                         Intermediate
                       </div>
                       <div className="text-[1rem] 2xl:text-[1.2rem] flex flex-row gap-2 items-center">
-                        <PersonFill/>
+                        <PersonFill />
                         3000+
                       </div>
                     </div>
+                    <div className="flex flex-row text-sm 2xl:text-base gap-1.5 flex-wrap">
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Python
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Tensorflow
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Pytorch
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Jupyter
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Linear Algebra
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Javascript
+                      </span>
+                      <span className="pl-2 pr-2 font-medium bg-gray-800 rounded-2xl">
+                        Github
+                      </span>
+                    </div>
                     <div className="flex flex-row gap-2">
-                    <button className="gradHighlight p-[0.5rem] text-sm lg:text-xl font-semibold max-w-fit">
+                      <button className="gradHighlight p-[0.5rem] text-sm lg:text-xl font-semibold max-w-fit">
                         <div className="flex flex-row items-center gap-2 text-sm">
-                            View Course <ArrowRightCircleFill />
+                          View Course <ArrowRightCircleFill />
                         </div>
-                    </button>
-                    <button className="gradHighlight p-[0.5rem] text-sm lg:text-xl font-semibold max-w-fit">
+                      </button>
+                      <button className="gradHighlight p-[0.5rem] text-sm lg:text-xl font-semibold max-w-fit">
                         <div className="flex flex-row items-center gap-2 text-sm">
-                            View Syllabus <ArrowRightCircleFill />
+                          View Syllabus <ArrowRightCircleFill />
                         </div>
-                    </button>
+                      </button>
                     </div>
                   </div>
                 </div>
