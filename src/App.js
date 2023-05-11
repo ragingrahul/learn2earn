1a40e1ff9368520bc8ab8fd74b98a7d27941af17
arihanv <arihanvaranasi@gmail.com>
2023-05-10 23:17:42 -0500

    Improved Responsiveness On Homepage

--- src/App.js
+++ src/App.js
@@ -5,60 +5,63 @@ import TopBar from "./TopBar";
 import { Button } from "react-bootstrap";
 import { ArrowRightCircleFill } from "react-bootstrap-icons";
 
-
 function App() {
   return (
     <div className="App">
       <div className="bgGrad">
         <TopBar />
         <header className="App-header">
-          <div className="grid sm:flex justify-center items-center">
-            <div className="text-left flex flex-col gap-8">
-              <div className="max-w-[650px]">
-                <h1 className="text-8xl">
+          <div className="grid items-center lg:flex gap-2 justify-center p-[5vw]">
+            <div className="flex flex-col gap-8 text-left">
+              <div className="md:max-w-[650px] sm:max-w-[80vw]">
+                <h1 className="text-6xl lg:text-8xl">
                   Get{" "}
-                  <span class="gradHighlight pr-4 pl-4 italic">
-                    Rewarded
-                  </span>
+                  <span class="gradHighlight pr-4 pl-4 italic">Rewarded</span>
                 </h1>
-                <h1 className="text-8xl">for Learning</h1>
-                <h4 className="text-3xl">
+                <h1 className="text-6xl lg:text-8xl">for Learning</h1>
+                <h4 className="lg:text-3xl">
                   Learn2Earn is an interesting platform that will teach you in
                   more an interactive way
                 </h4>
               </div>
               <div className="flex flex-row gap-10">
-                <button className="gradHighlight p-[0.9rem] text-2xl font-semibold">
-                  <div className="flex flex-row items-center gap-2">Go to Courses <ArrowRightCircleFill /></div>
+                <button className="gradHighlight p-[0.9rem] text-lg lg:text-2xl font-semibold">
+                  <div className="flex flex-row items-center gap-2">
+                    Go to Courses <ArrowRightCircleFill />
+                  </div>
                 </button>
-                <button className="gradHighlight p-[0.9rem] text-2xl font-semibold">
-                <div className="flex flex-row items-center gap-2">Go to Specializations <ArrowRightCircleFill /></div>
+                <button className="gradHighlight p-[0.9rem] text-lg lg:text-2xl font-semibold">
+                  <div className="flex flex-row items-center gap-2">
+                    Go to Specializations <ArrowRightCircleFill />
+                  </div>
                 </button>
               </div>
             </div>
+            <div className="flex justify-center">
             <img src={logo} className="App-logo" alt="logo" />
+            </div>
           </div>
         </header>
       </div>
-      <div className="gradHeader">
+      <div className="gradHeader max-h-[300px] gap-5 md:gap-15 lg:gap-20 lg:h-[25vh] p-[1vw]">
         <div>
-          <div className="text-lg font-bold">Students Helped</div>
+          <div className="font-bold md:text-lg">Students Helped</div>
           <div className="text-2xl font-bold">20,000+</div>
         </div>
         <div>
-          <div className="text-lg font-bold">Courses</div>
+          <div className="font-bold md:text-lg">Courses</div>
           <div className="text-2xl font-bold">150+</div>
         </div>
         <div>
-          <div className="text-lg font-bold">Specializations</div>
+          <div className="font-bold md:text-lg">Specializations</div>
           <div className="text-2xl font-bold">50+</div>
         </div>
         <div>
-          <div className="text-lg font-bold">Instructors</div>
+          <div className="font-bold md:text-lg">Instructors</div>
           <div className="text-2xl font-bold">100+</div>
         </div>
         <div>
-          <div className="text-lg font-bold">Total Redeemed</div>
+          <div className="font-bold md:text-lg">Total Redeemed</div>
           <div className="text-2xl font-bold">$1.52M</div>
         </div>
       </div>
