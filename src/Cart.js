586cf81799fc9d51a0014d469b77a4bb7cd3bdf0
arihanv <arihanvaranasi@gmail.com>
2023-05-24 00:06:35 -0500

    Created Total for Carts Page and Estimated Earning

--- src/Cart.js
+++ src/Cart.js
@@ -9,28 +9,14 @@ import { clearCart, removeFromCart } from "./redux/actions";
 function Cart() {
   const dispatch = useDispatch();
   const reduxStore = useSelector((state) => state);
-  const tCart = [
-    {
-      name: "Machine Learning",
-      price: "$29.99",
-    },
-    {
-      name: "Web Development",
-      price: "$59.99",
-    },
-    {
-      name: "App Development",
-      price: "$69.99",
-    },
-  ];
 
   return (
     <div className="App">
       <div className="bgGrad">
         <TopBar />
         <header className="App-header">
-          <div className="w-fit min-h-[90vh] sm:w-[80vw] max-w-[1400px] mt-[3.5rem] h-full flex flex-col gap-3 drop-shadow-2x">
-            {Object.keys(reduxStore.cart.cart).length !== 0 && (
+          <div className="w-fit min-h-[94vh] sm:w-[80vw] max-w-[1400px] mt-[3.5rem] h-full flex flex-col gap-3 drop-shadow-2x">
+            {Object.keys(reduxStore.cart.cart).length !== 0 ? (
               <>
                 <div className="italic text-left font-semibold text-6xl p-2 text-gray-100 flex items-center gap-4">
                   Cart
@@ -41,10 +27,12 @@ function Cart() {
                     Clear All
                   </div>
                 </div>
-                <div className=" bg-gray-900 rounded-xl text-left p-3">
-                  <div className="grid grid-cols-[2fr,1fr] pb-2">
+                <div className="bg-gray-900 rounded-xl text-left p-3">
+                  <div className="grid grid-cols-[2fr,1fr]">
                     <div className="font-semibold text-lg">Product</div>
-                    <div className="font-semibold text-lg">Product</div>
+                    <div className="font-semibold text-lg border-gray-700 h-full items-center border-l pb-2 pl-5">
+                      Price
+                    </div>
                   </div>
                   <div className="text-2xl">
                     {Object.values(reduxStore.cart.cart).map((course) => (
@@ -63,8 +51,16 @@ function Cart() {
                           />
                           {course.name}
                         </div>
-                        <div className="font-semibold text-4xl flex justify-between pr-4">
-                          {course.price}
+                        <div className="font-semibold text-4xl flex justify-between pr-4 border-l border-gray-700 h-full items-center pl-5">
+                          <div className="flex flex-col">
+                            {course.price}
+                            <div className="text-base italic text-gray-400">
+                              Completion Earnings:{" "}
+                              <span className="text-blue-300">
+                                ${parseFloat(course.price.slice(1)) * 1.5}
+                              </span>
+                            </div>
+                          </div>
                           <motion.button
                             className="text-gray-600 text-2xl flex items-center"
                             whileHover={{ scale: 1.1 }}
@@ -85,6 +81,51 @@ function Cart() {
                     ))}
                   </div>
                 </div>
+                <div className="bg-gray-900 rounded-xl text-left p-3">
+                  <div className="font-semibold text-lg border-b border-gray-700 grid grid-cols-[2fr,1fr]">
+                    <div className="pb-2">Summary</div>
+                    <div className="border-gray-700 h-full items-center border-l pb-2 pl-5">
+                      Total Price
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-[2fr,1fr] items-center font-semibold">
+                    <div>
+                      {Object.values(reduxStore.cart.cart).length} Courses
+                    </div>
+                    <div className="border-l border-gray-700 h-full items-center pl-5 pr-4 pt-2">
+                      <div>
+                        $
+                        {parseFloat(
+                          Object.values(reduxStore.cart.cart)
+                            .reduce((a, b) => {
+                              const result = a + parseFloat(b.price.slice(1));
+                              return result;
+                            }, 0)
+                            .toFixed(2)
+                        )
+                          .toString()
+                          .padEnd(6, "0")}
+                      </div>
+                      <div className="text-base italic text-gray-400 -mt-2">
+                        Total Completion Earnings:{" "}
+                        <span className="text-green-300 text-xl">
+                          $
+                          {parseFloat(
+                            Object.values(reduxStore.cart.cart)
+                              .reduce((a, b) => {
+                                const result =
+                                  a + parseFloat(b.price.slice(1)) * 1.5;
+                                return result;
+                              }, 0)
+                              .toFixed(2)
+                          )
+                            .toString()
+                            .padEnd(6, "0")}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
                 <motion.button
                   whileHover={{ scale: 1.1 }}
@@ -99,6 +140,28 @@ function Cart() {
                   </div>
                 </motion.button>
               </>
+            ) : (
+              <div className="justify-center items-center flex h-[80vh] flex-col gap-3 font-semibold">
+                <div>No items in cart</div>
+                <div className="flex flex-row gap-10">
+                  <button className="gradHighlight p-[0.9rem] text-lg lg:text-2xl font-semibold hover:bg-black">
+                    <div
+                      className="flex flex-row items-center gap-2"
+                      onClick={() => (window.location.href = "#/courses")}
+                    >
+                      Go to Courses <ArrowRightCircleFill />
+                    </div>
+                  </button>
+                  <button className="gradHighlight p-[0.9rem] text-lg lg:text-2xl font-semibold hover:bg-black">
+                    <div
+                      className="flex flex-row items-center gap-2"
+                      onClick={() => (window.location.href = "#/specials")}
+                    >
+                      Go to Specializations <ArrowRightCircleFill />
+                    </div>
+                  </button>
+                </div>
+              </div>
             )}
           </div>
         </header>
